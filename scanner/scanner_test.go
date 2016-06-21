fb049d7aab7bc59bd779b01d60ee5465dcfaf67c
Jeremy Bowers <jerf@jerf.org>
2016-06-21 09:07:19 -0400

    Remove debugging output in test suite.

--- scanner/scanner_test.go
+++ scanner/scanner_test.go
@@ -1,4 +1,4 @@
-// Copyright 2012 The Gorilla Authors, Copyright 2015 Barracuda Networks.
+// Copyright as given in CONTRIBUTORS
 // All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
